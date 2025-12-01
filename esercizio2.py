0d8e2ca0217d899bcdee3e6af0323e43162e4d1a
ke31731-code <ke31731@gmail.com>
2025-12-01 13:37:37 +0100

    Examples of Datatypes
    Solved  first exercises from the module1( datatypes)

---
+++ esercizio2.py
@@ -0,0 +1,26 @@
+#Esercizio 1
+
+x = 7
+y = 3
+somma = x + y 
+print(f"La somma dei due numeri è: {somma}")
+
+#Esercizio 2 
+
+prezzo = 19.99
+quantità = 3 
+spesa = (prezzo * quantità)
+print(f"La tua spesa è di {spesa} euro")
+
+#Esercizio 3 
+x = 10
+if x > 5:
+    print("True")
+else:
+    print("False")
+
+#Esercizio 4
+
+a = 3
+b = 2.5
+print(a+b)
